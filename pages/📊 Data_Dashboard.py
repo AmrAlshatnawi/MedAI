2d125c4a79f33d5250d3339e23a41bbd38720e55
Farhan Masood <farhan.mk21@gmail.com>
2024-02-28 11:01:26 -0600

    Added variable selectors

--- pages/📊 Data_Dashboard.py
+++ pages/📊 Data_Dashboard.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import streamlit as st
 from functions import *
@@ -6,6 +7,7 @@ from functions import *
 st.set_page_config(
         page_title="MedAI - Data",
         page_icon="📊",
+        layout = 'wide'
     )
 # Make page content larger (zoom)
 st.markdown("""<style>body {zoom: 1.5;  /* Adjust this value as needed */}</style>""", unsafe_allow_html=True)
@@ -18,5 +20,24 @@ if not st.session_state['authenticated']:
 # Show page if user is authenticated
 if st.session_state['authenticated']: 
     # #page content start here 
-    st.write("Page is still under development.")
     uploaded_file = st.file_uploader("Please upload a csv file.")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        numeric_df = df.select_dtypes(['float','int'])
+        numeric_cols = numeric_df.columns
+
+        text_df = df.select_dtypes(['object'])
+        text_cols = text_df.columns
+
+        with st.expander("Data Preview"):
+            st.dataframe(df)
+        list_of_cols = list(df.columns)
+        option_x = st.selectbox(
+        'Select x-variable',
+        numeric_cols)
+
+        option_y = st.selectbox(
+        'Select y-variable',
+        numeric_cols)
+        
+        st.write(f'You selected: {option_y} against {option_x}')
